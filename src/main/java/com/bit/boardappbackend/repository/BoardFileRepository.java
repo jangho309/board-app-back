9cb6c26c5c2f0b53585a49acdad540e383f170cf
DESKTOP-B949T0J\jangho <jangho309@naver.com>
2024-09-27 10:44:08 +0900

    board-app-back

---
+++ src/main/java/com/bit/boardappbackend/repository/BoardFileRepository.java
@@ -0,0 +1,8 @@
+package com.bit.boardappbackend.repository;
+
+import com.bit.boardappbackend.entity.BoardFile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardFileRepository extends JpaRepository<BoardFile, Long> {
+    void deleteByBoardId(Long id);
+}
